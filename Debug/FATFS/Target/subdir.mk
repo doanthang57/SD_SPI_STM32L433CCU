2fb68149f45f00047812f7092d23e3b9ac4f7c38
doanthang57 <doanthang57@gmail.com>
2020-06-17 17:50:34 +0700

    SD using SPI1

---
+++ Debug/FATFS/Target/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../FATFS/Target/user_diskio.c 
+
+OBJS += \
+./FATFS/Target/user_diskio.o 
+
+C_DEPS += \
+./FATFS/Target/user_diskio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+FATFS/Target/user_diskio.o: ../FATFS/Target/user_diskio.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32L433xx -c -I../FATFS/App -I../Drivers/CMSIS/Include -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../FATFS/Target -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Middlewares/Third_Party/FatFs/src -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"FATFS/Target/user_diskio.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
